a7174875ca06f84d66c10a344f4a0aaa1a59a34c
SaikumarChintada <chintadasaikumar@gmail.com>
2017-12-17 23:38:35 +0530

    add chrome extension test proj

---
+++ chrome-extension/bg.js
@@ -0,0 +1,49 @@
+// todo: 
+// + message passing request object  as a seperate interface
+// + use async/promises 
+// + use mvc
+
+
+initStorage();
+
+// Event 1:  on receiving a url from content script
+// store url, maintain cache ??, send success
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.event == "url.new"){
+		setUrl(request.data,sender.tab.id);
+	}
+});
+
+// Event 2 : console.log the urlArray from localstorage
+chrome.browserAction.onClicked.addListener(function(tab) {
+	getAllUrls((item) => chrome.tabs.sendMessage(tab.id, {event: "ext.click",data : item})); 
+});
+
+
+
+// definitions
+function initStorage(){
+	chrome.storage.local.get('urls', (item) => {
+		if(!item){
+			StorageArea.set({"urls" : []});
+		}
+	});
+}
+function getAllUrls(cb){
+	chrome.storage.local.get('urls', (item) => cb(item));
+}
+function setUrl(data,tab_id){
+	chrome.storage.local.get('urls',(item) => {
+		if(item){
+			var updatedUrls = item.urls || [];
+			updatedUrls.push(data);
+			chrome.storage.local.set({"urls" : updatedUrls},() => {
+				var success = true;
+				if (typeof chrome.runtime.lastError !== 'undefined') {
+					success = false;
+				}
+				chrome.tabs.sendMessage(tab_id,{event : "url.save",data: success});
+			});
+		}
+	});
+}
---
+++ chrome-extension/cs.js
@@ -0,0 +1,37 @@
+(function(){
+	//config
+	const CS_DIV = '__my__simple__cxt__wrapper__';
+
+	//setup 
+	var loc = window.location.href;
+	var div = document.createElement('div');
+	div.id = CS_DIV;
+	div.setAttribute('style','position:fixed;top:20px;font:30px;z-index:10;background:yellow');
+	document.body.appendChild(div);
+
+	//chrome ext events -------------------
+
+	//emmit new url
+	chrome.runtime.sendMessage({event : "url.new",data : loc});
+
+	// handler ext click  : receive all urls to log
+	// handle url save response : console log result
+	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		if (request.event == "ext.click"){
+			addTextToPage();
+			console.log(request.data);
+		}else if(request.event == "url.save"){
+			console.log({logSuccess : request.data});
+		}
+	});
+
+	//helpers -----------------------------
+	function addTextToPage(){
+		var extDiv = document.getElementById(CS_DIV);
+		var span = document.createElement('div');
+		span.setAttribute('style','position:relative');
+	    span.innerText = "Horray! my extension clicked ..";
+	    extDiv.appendChild(span);
+	}
+
+})(chrome);
